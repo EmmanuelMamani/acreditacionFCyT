7f600c307b60e2ed96d0285ca16a1b9e5440461a
DafneXCA <cruzdafne123@gmail.com>
2023-06-27 21:12:01 -0400

    cambio de letra en el login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Gruppo&family=Raleway:wght@100&family=Space+Grotesk:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&family=Raleway:wght@200&family=Tajawal:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/login.css')}}">
     <title>Sis-EA</title>
 </head>
@@ -25,23 +26,23 @@
         <div id="formulario">
             <form action="{{route('login')}}" method="post">
                 @csrf
-                <h4>Inicio de sesion</h4>
-            <label for="email">Correo electronico</label>
+                <h4>Inicio de sesión</h4>
+            <label for="email" class="mensajes">Correo electrónico</label>
             <div class="input-container">
                 <i class="fas fa-envelope"></i>
                     <input type="text" name="email" value="{{old('email')}}">
                   
             </div>
             @error('email')
-            <span class="text-red-700"> {{ $message }}</span><br> 
+            <span class="error " > {{ $message }}</span><br> 
             @enderror
-            <label for="password">Contraseña</label>
+            <label for="password" class="mensajes">Contraseña</label>
             <div class="input-container">
                 <i class="fas fa-key"></i>
                     <input type="password" name="password">
             </div>
             @error('password')
-                    <span class="text-red-700"> {{ $message}}</span><br>
+                    <span class="error"> {{ $message}}</span><br>
                     @enderror
             <input type="submit" value="Entrar" id="acceder">
             </form>
